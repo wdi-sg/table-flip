83f8a4bf5f2dcc36e91ef26302529cd5233b23da
sharkyu3 <lowhuiyu@hotmail.com>
2019-06-27 16:54:56 +0800

    added files to empty repo

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("hello script js");
+
+// greetUser
+
+// data description
+// currentInput - User's input
+
+// input example
+// var currentInput = "Joan";
+
+// function signature
+// currentInput (string) --> inputHappened (string)
+
+// function purpose
+// To greet hello if user inputs hello. To greet hello plus input if any other string input. To show error message if input is not a string.
+
+// function header
+//inputHappened( currentInput )
+
+// functional examples
+// var inputHappened = function( "Hello" ); // will equal "Hello!"
+// var inputHappened = function( "Joan" ); // will equal "Hello Joan"
+// var inputHappened = function( "123" ); // will equal "Invalid input"
+
+var checkIfString = function(string){
+    if(typeof string ===string){
+        display "Hello"
+    }
+}
+
+var inputHappened = function(currentInput){
+    console.log( currentInput);
+    display(message)
+};
+
+var currentInput = function(currentInput) {
+
+}
