e971482c03f78cae85bf58b65998fbc8d132b0b7
Yi Xin Ang <angyixin@gmail.com>
2019-06-27 23:22:37 +0800

    game logic in js. first commit of html and css too.

---
+++ script.js
@@ -0,0 +1,65 @@
+console.log("hello script js");
+
+var inputHappened = function(currentInput){
+  console.log( currentInput );
+  display( currentInput );
+}
+
+  var secretWord = ["c","a","t"];
+  var wrongGuesses = 0;
+  var correctGuesses = 0;
+  var letterFound = false;
+  var theLetter;
+  var flipTable = ["┳━┳"," (ರ ~ ರ）┳━┳", "(╯ರ ~ ರ）╯︵ ┻━┻"];
+
+  // when user takes a turn and inputs a character
+  //checkForMatch("z");
+  //checkForMatch(input);
+
+  var checkForMatch = function(input){
+
+  // look through the word to guess. is the input letter there?
+  // use a value set outside the searching loop to know if the letter was found
+
+    var i = 0;
+    while ( i < secretWord.length){
+      console.log( `${i} ${secretWord[i]}`);
+      // if the guess right
+      if(secretWord[i] === input){
+        // add it to correctly guessed numbers
+        // the message is you guessed right. show the letters they have so far. (no need to show them in the order they appear in the word- just show them in the order guessed)
+        // otherwise the guess is wrong
+        // keep track of the number of wrongly guessed letters
+        // take one character off the table flip and add it to the current characters
+        // the message includes the current table flip characters
+        // see if the game has ended because of too many wrong guesses
+        // if it has include in the message the game is over
+        // show the output to the user
+        console.log("match");
+        theLetter = input;
+        letterFound = true;
+      } else {
+        console.log("no match");
+      }
+      i = i + 1;
+    }
+
+    // check if letter found
+     if( letterFound === true){
+      console.log("1 point for the letter " + theLetter);
+      correctGuesses = correctGuesses + 1;
+      console.log("total points: " + correctGuesses);
+      //turn this switch back;
+      letterFound = false;
+      }else{
+      console.log("NOT FOUND");
+      wrongGuesses = wrongGuesses + 1;
+      console.log("total wrong guesses: " + wrongGuesses);
+      console.log(wrongGuesses -1);
+      console.log(flipTable[(wrongGuesses-1)]);
+    }
+
+    if( wrongGuesses === flipTable.length ){
+      console.log("game ends");
+    }
+  }
