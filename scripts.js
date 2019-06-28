8db0438a7017c2d65fcc0bde1fa26e5d58402616
Janet <cashhpvn@yahoo.com>
2019-06-28 12:51:57 +0800

    update script.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,36 @@
 console.log("hello script js");
+var secretWord = "cat";
+var secretChar = secretWord.split('');
+var correctChar = [];
+var numOfTries = 5;
+var inputFound = false;
 
+	
+
+function check(data) {
+	if(data > 0) {
+		display(`You've got ${numOfTries} left.`)
+	} else {
+		display("Game Over!")
+	}
+}
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  display( "WOW SOMETHING HAPPENED" );
+	numOfTries -= 1;
+
+	for(let i = 0; i < secretWord.length; i ++) {
+		if (currentInput === secretChar[i]){
+			inputFound = true;
+		}
+	}
+	if(inputFound = true){
+		correctChar.push(currentInput);
+		console.log(correctChar);
+	} else {
+	
+		check(numOfTries);
+	}
+
+  
+
 };
