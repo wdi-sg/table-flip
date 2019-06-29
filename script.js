533587c0ba48ba45ef72fdccfab47bbe8c886dcc
Sarah Choo <sarahchoo@Sarahs-MacBook-Pro.local>
2019-06-29 16:45:16 +0800

    Completed part one guessing word cat

--- script.js
+++ script.js
@@ -1,52 +1,66 @@
 console.log("hello world");
 
+var secretWord = ["c","a","t"];
 
-/*  if (totalNumOfWrongGuesses === true) {
-    display ("You lost the guessing game")
-    */
+var tableFlip = ["(╯ರ ~ ರ）╯","︵", "┻━┻"];
 
-
-// table-flip emoji ["(", "╯", "°□°", ")", "╯", "︵", "┻━┻"];
-
-// var wrongGuessCharacterAppear = function();
-//     numOfWrongGuesses = numOfWrongGuesses + 1;
-
-// var wrongCharacters = ["b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z"];
-
-
-
-var secretWord = ["c", "a", "t"];
-
-var correctGuesses = [];
-var wrongGuesses = [];
+var correctGuesses = '';
+var wrongGuesses = '';
 
 
 var inputHappened = function(currentInput){
 
+    //converts uppercase input word to lowercase
+    var lowerCaseInput = currentInput.toLowerCase();
 
-for (i = 0; i < secretWord.length; i++)
+//var found has to be local variable
+    var found = false
 
-    if (currentInput === secretWord[i]) {
-        console.log ("Correct Answer");
-        correctGuesses = correctGuesses + 1;
-        console.log("You guessed the correct character");
-}
-    else if (currentInput != secretWord[i]) {
-        console.log("Wrong Answer")
-        wrongGuesses = wrongGuesses + 1;
-        console.log("Wrong character. Try again");
+for (var i = 0; i < secretWord.length; i++) {
+        if (lowerCaseInput === secretWord[i] && secretWord.length > 1) {
+        //correctGuesses = correctGuesses + secretWord.splice(i,1)
+        //splice means revoke word from secretWord array
+        correctGuesses += secretWord.splice(i,1)
+        //console.log(correctGuesses)
+        display("You guessed the correct character " + correctGuesses);
+        found = true;
+        //When the break statement is used in a loop, it breaks the loop and continues executing the code after the loop
+        break;
+
+    } else if (lowerCaseInput === secretWord[i] && secretWord.length === 1) {
+        // correctGuesses.push(secretWord[i])
+        correctGuesses += secretWord.splice(i,1)
+        //var secretWord array is empty now
+        display("You win. You guessed cat ")
+        found = true;
     }
+}
 
+//wrongGuesses.length < 5 means 4 chances to play
+    if (found === false && wrongGuesses.length < 5) {
+
+//+= means wrongGuess = wrongGuesses + tableFlip
+//shift() method removes the first element from tableFlip array and returns that removed element
+        wrongGuesses += tableFlip.shift()
+        //console.log(wrongGuesses)
+        display("Wrong guess " + wrongGuesses);
+//if you guess 4 times wrong, you lose
+    } else if (found === false && wrongGuesses.length >4) {
+        wrongGuesses += tableFlip.shift();
+        display("Game over " + wrongGuesses)
+    }
 };
 
 
-// Store number of wrong guesses to a variable
-var totalNumOfWrongGuesses = 10;
 
 
-// Store number of correct guesses to a variable
 
-var totalNumberOfCorrectGuesses = function() {
-    correctGuesses 3;
-    console.log(correctGuesses);
-}
+
+
+
+
+
+//var wrongGuess = [];
+//wrongGuesses.push(currentInput) stores wrong guesses in array
+
+// var wrongCharacters = ["b", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "u", "v", "w", "x", "y", "z"];
