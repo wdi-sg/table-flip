5d11c2f4a57b3b65cbdb6a1308de8587e12f5b06
Stuart Myers <stuart.myers@gmail.com>
2019-12-12 20:54:54 +0800

    Completed Part 5

--- script.js
+++ script.js
@@ -23,17 +23,18 @@ var words = ['cat','rat','doggy','armadillo']
 
 var gameLevel = 0;
 
-var secretWord = words[gameLevel].split('');
+var secretWord = words[gameLevel];
 
 var tableFlipWord = "(╯°□°）╯︵ ┻━┻"
 var tableFlip = tableFlipWord.split('');
 var tableFlipRevealed = [];
 
 // store incorrectly guessed letters in here:
-var incorrectGuesses = [];
+var incorrectLetters = [];
 
-// store correctly guessed letters in here:
-var correctGuesses = [];
+// store correctly guessed letters in here.
+// The level is won when correctLetters equals secretWord.
+var correctLetters = [];
 
 var outputToPlayer = "";
 
@@ -41,32 +42,58 @@ var gameOver = false;
 
 var restartString = "restart";
 
+// regular expression to filter out letters.
 var letter = /^[a-z]+$/;
+
+var gameRevealedLettersString = "";
+
+var tableFlipGuyString = ""
+
+var happyEmoticons = ["(*＾▽＾)／ Go you!", "d=(´▽｀)=b So happy!", "(◕‿◕✿)", "ᕕ( ᐛ )ᕗ Let's go!", "༼ つ ◕_◕ ༽つ Giff Energy!"];
+
+var sadEmoticons = ["( ._.)", "( ⚆ _ ⚆ )", "⊙﹏⊙", "T_T", "⊂(⊙д⊙)つ", "(^^;)"];
+
+
+var randomEmoticon = function(happy){
+    if (happy){
+        return happyEmoticons[Math.floor(Math.random() * happyEmoticons.length)];
+    } else {
+        return sadEmoticons[Math.floor(Math.random() * sadEmoticons.length)];
+    }
+}
+
 
 var resetGame = function() {
     secretWord = words[gameLevel];
     tableFlip = tableFlipWord.split('');
     tableFlipRevealed = [];
-    incorrectGuesses = [];
-    correctGuesses = [];
+    incorrectLetters = [];
+    correctLetters = [];
+    for (var i = secretWord.length - 1; i >= 0; i--) {
+        correctLetters.push("_")
+    }
+    gameRevealedLettersString = correctLetters.join(" ");
+    tableFlipGuyString = "";
     gameOver = false;
 }
+
 
 var checkWinState = function() {
     /*
     if letters found = secret Word length you win the game
     if not, carry on. */
-    if (correctGuesses.length === secretWord.length) {
+    if (correctLetters.join('') === secretWord) {
         gameLevel++;
         if (gameLevel === words.length) {
-            outputToPlayer += "You've completed the game! Please type " + restartString + " to play again.";
+            outputToPlayer += "\nYou've won the game!\nPlease type " + restartString + " to play again.";
             gameOver = true;
         } else {
-            outputToPlayer += "\nYou win! Onto level " + (gameLevel + 1);
+            outputToPlayer += "\nYou got " + secretWord + ". Onto level " + (gameLevel + 1) + "! " + randomEmoticon(true);
             resetGame();
         }
     }
 }
+
 
 var checkLoseState = function() {
     /* if incorrect Guesses = length of table flip. You lose the game.
@@ -77,23 +104,25 @@ var checkLoseState = function() {
     }
 }
 
+
 var isGuessedAlready = function(letterToCheck) {
 
     // if a letter is in the correct Guesses or incorrect Guesses then return True:
 
-    for (var i = 0; i < correctGuesses.length; i++) {
-        if (correctGuesses[i] === letterToCheck) {
+    for (var i = 0; i < correctLetters.length; i++) {
+        if (correctLetters[i] === letterToCheck) {
             return true;
         }
     }
-    for (var i = 0; i < incorrectGuesses.length; i++) {
-    if (incorrectGuesses[i] === letterToCheck) {
+    for (var i = 0; i < incorrectLetters.length; i++) {
+    if (incorrectLetters[i] === letterToCheck) {
             return true;
         }
     }
 
     return false;
 }
+
 
 var checkGuess = function(letterToCheck) {
     /* is the guessed letter in the secret word? Return a success if true. */
@@ -112,8 +141,8 @@ var checkGuess = function(letterToCheck) {
     for (var i = 0; i < secretWord.length; i++) {
         if (secretWord[i] === letterToCheck) {
             letterFound = true;
-            outputToPlayer += 'letter ' + letterToCheck + ' found!';
-            correctLetter(letterToCheck);
+            outputToPlayer = 'letter ' + letterToCheck + ' found!';
+            correctLetter(letterToCheck, i);
         }
     }
 
@@ -121,6 +150,7 @@ var checkGuess = function(letterToCheck) {
         wrongLetter(letterToCheck);
     }
 }
+
 
 var validateGuess = function(guessedLetter) {
     if (guessedLetter.length === 1) {
@@ -132,22 +162,25 @@ var validateGuess = function(guessedLetter) {
     return false;
 }
 
-var correctLetter = function(guessedLetter) {
+
+var correctLetter = function(guessedLetter, indexInWord) {
     /* Guess is correct! */
-    correctGuesses.push(guessedLetter);
-    console.log('Current correct guesses: ' + correctGuesses);
+    correctLetters[indexInWord] = guessedLetter;
+    gameRevealedLettersString = correctLetters.join(" ");
     checkWinState();
 }
+
 
 var wrongLetter = function(guessedLetter) {
     /* Guess is wrong! */
     tableFlipRevealed.push(tableFlip.shift());
-    incorrectGuesses.push(guessedLetter);
-    outputToPlayer += guessedLetter + " is a wrong guess! \n";
-    outputToPlayer += tableFlipRevealed.join('');
-    console.log('Incorrect Guesses so far: ');
+    incorrectLetters.push(guessedLetter);
+    outputToPlayer += guessedLetter + " is a wrong guess! " + randomEmoticon(false) + "\n";
+    tableFlipGuyString = tableFlipRevealed.join('');
+    // console.log('Incorrect Guesses so far: ');
     checkLoseState();
 }
+
 
 var inputHappened = function(currentInput){
     outputToPlayer = "";
@@ -158,7 +191,7 @@ var inputHappened = function(currentInput){
         if (currentInput === restartString) {
             gameLevel = 0;
             resetGame();
-            return "┬──┬◡ﾉ(° -°ﾉ)\nrestarting. Please enter your first guess for a new game.";
+            return gameRevealedLettersString + "\n┬──┬◡ﾉ(° -°ﾉ)\nrestarting. Please enter your first guess for a new game.";
         }
 
         return "Game is over please type " + restartString + " to restart";
@@ -166,5 +199,8 @@ var inputHappened = function(currentInput){
 
     console.log( "Input character: " + currentInput );
     checkGuess(currentInput);
-    return outputToPlayer;
+    return gameRevealedLettersString + "\n" + tableFlipGuyString + "\n" + outputToPlayer;
 };
+
+document.querySelector('#output').innerText = "Make a guess to begin the game.";
+resetGame();
