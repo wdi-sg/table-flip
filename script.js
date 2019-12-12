d765856276b1b6e4ff04e67b8904b6d7e769bb8a
benjacoblee <benjacoblee@gmail.com>
2019-12-12 17:47:54 +0800

    implemented win condition, lose condition

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
+// var words = [
+//   ["c", "a", "t"],
+//   ["d", "o", "g", "g", "y"],
+//   ["a", "l", "p", "h", "a", "b", "e", "t"]
+// ];
+
 let catArray = ["c", "a", "t"];
 let flipTableArray = ["(", "╯", "ರ", "~", "ರ", "）", "╯", "︵", "┻", "━", "┻"];
-let catWord = "";
-let flipTableWord = "";
-let flipIndex;
+let letterFound = false;
+let correctlyGuessedLetters = "";
+let numberOfCorrectlyGuessedLetters = 0;
+let numberOfWronglyGuessedLetters = 0;
+let flipTableFigure = "";
+let input = document.querySelector("#input");
 
 console.log("hello script js");
 
 var inputHappened = function(currentInput) {
   for (let i = 0; i < catArray.length; i++) {
     if (currentInput === catArray[i]) {
-      catWord = catWord + catArray[i];
-      return catWord;
-    } 
+      letterFound = true;
+      numberOfCorrectlyGuessedLetters++;
+      if (letterFound && numberOfCorrectlyGuessedLetters !== catArray.length) {
+        letterFound = false;
+        correctlyGuessedLetters = correctlyGuessedLetters + catArray[i];
+        input.value = "";
+        return `You guessed right! Correctly guessed letters: ${correctlyGuessedLetters}`;
+      } else {
+        return `You won!`;
+      }
+    }
+  }
+  input.value = "";
+
+  numberOfWronglyGuessedLetters++;
+  flipTableFigure += flipTableArray.shift();
+  if (flipTableArray.length > 0) {
+    return flipTableFigure;
+  } else {
+    return `Sorry! You lost. 
+              (╯ರ ~ ರ）╯︵ ┻━┻`;
   }
 };
