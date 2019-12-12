293ced0c170f14438ae1d8db4a81cfc70cc641af
SafrazHakamali <safrazhakamali@gmail.com>
2019-12-12 23:52:36 +0800

    completed further 6: admin mode. EVERYTHING'S DONE WOOOHOO!

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("hello script js");
 
 var presplitWords = ['cat','doggy','alphabet'];
+var numberOfWords = presplitWords.length;
 var words=[];
 for (i=0; i<presplitWords.length; i++) {
 	console.log(i);
@@ -18,11 +19,27 @@ var wrong = true;
 var notify;
 var hangmanTrack = [];
 var incorrects = 0;
-
+var adminCount = 0;
 var inputHappened = function(currentInput){
   notify = "You have guessed wrong.";
   console.log( currentInput );
-  for (i=0; i<words[whichWord].length; i++) {
+  if (currentInput=="admin" && adminCount == 0) {
+  	adminCount = 1;
+  	document.querySelector('#input').value = ""
+  	return "Admin mode entered. Enter additional words to guess";
+  } else if (adminCount ==1 && currentInput== "endadmin") {
+  	adminCount = 0;
+  	document.querySelector('#input').value = ""
+  	return "Out of admin mode. Continue";
+  } else if (adminCount == 1) {
+  	presplitWords.push(currentInput);
+  	console.log(presplitWords);
+  	numberOfWords = presplitWords.length;
+	words.push(presplitWords[presplitWords.length-1].split(''));
+  	document.querySelector('#input').value = "";
+  	return "Added the word "+ currentInput;
+  } else {
+  	for (i=0; i<words[whichWord].length; i++) {
   	if (currentInput==words[whichWord][i]) {
   		correctGuess.push(words[whichWord][i]);
   		guessing[i]=currentInput;
@@ -48,7 +65,7 @@ var inputHappened = function(currentInput){
   document.querySelector('#input').value = ""
   if (correctGuess.length<words[whichWord].length && incorrects<3) {
   	return notify + " So far you have guessed " + correctGuess.length + " letters correctly. " + guessing.join('') +". Table-flip: " + hangmanTrack;
-  } else if (correctGuess.length==words[whichWord].length && whichWord<2){
+  } else if (correctGuess.length==words[whichWord].length && whichWord<(numberOfWords-1)){
   	whichWord++;
   	correctGuess = [];
   	guessing = [];
@@ -56,9 +73,10 @@ var inputHappened = function(currentInput){
 	guessing.push('_');
 	}
 	return "You guessed the word "+ words[whichWord-1].join('')+"! Guess the next word."
-  } else if (correctGuess.length==words[whichWord].length && whichWord==2){
+  } else if (correctGuess.length==words[whichWord].length && whichWord==(numberOfWords-1)){
   	return "You won by guessing the last word "+words[whichWord].join('')+"! Table-flip: " + hangmanTrack;
   } else {
   	return "You lose! Table-flipped!: " + hangmanTrack;
   }
+}
 }
