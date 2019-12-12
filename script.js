0872e71eccd2578934aee4eabd2435cbdbb61602
Robert Kolsek <kolsek.income@gmail.com>
2019-12-12 17:46:47 +0800

    First part done

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-console.log("hello script js");
+let secretWord = ['c', 'a', 't']
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPEND";
-};
+let tableFlip = ["(", "╯", "ರ", " ", "~", " ", "ರ", ")", " ", "╯", "︵", " ", "┻", "━", "┻"]
+const gameLength = tableFlip.length
+
+let currentTableFlip = []
+
+let correctLetter = ""
+
+let wrongGuesses = 0
+
+var inputHappened = function (currentInput) {
+
+  for (let i = 0; i < secretWord.length; i++) {
+    if (currentInput === secretWord[i]) {
+        correctLetter = correctLetter + currentInput
+        return `You guessed it RIGHT! - Your correct guesses: ${correctLetter}`
+    }
+  }
+      let shiftedCharacter = tableFlip.shift() 
+      currentTableFlip.push(shiftedCharacter)
+      wrongGuesses++
+      if (wrongGuesses > gameLength) {
+        return `Game over!`
+      } else {
+        return `${currentTableFlip.join("")}`
+      }
+      
+}
