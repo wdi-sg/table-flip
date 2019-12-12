c0f647495f4f84b8825555ace6685a74ca946e51
benjacoblee <benjacoblee@gmail.com>
2019-12-12 23:43:57 +0800

    added array of arrays

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
-// var words = [
-//   ["c", "a", "t"],
-//   ["d", "o", "g", "g", "y"],
-//   ["a", "l", "p", "h", "a", "b", "e", "t"]
-// ];
-
-let catArray = ["c", "a", "t"];
-let flipTableArray = ["(", "╯", "ರ", "~", "ರ", "）", "╯", "︵", "┻", "━", "┻"];
+const words = [
+  ["c", "a", "t"],
+  ["d", "o", "g", "g", "y"],
+  ["a", "l", "p", "h", "a", "b", "e", "t"]
+];
+const flipTableArray = ["(", "╯", "ರ", "~", "ರ", "）", "╯", "︵", "┻", "━", "┻"];
 let letterFound = false;
 let correctlyGuessedLetters = "";
 let numberOfCorrectlyGuessedLetters = 0;
 let numberOfWronglyGuessedLetters = 0;
 let flipTableFigure = "";
-let input = document.querySelector("#input");
+const numberOfArrayLetters = words[0].length + words[1].length + words[2].length;
+const input = document.querySelector("#input");
+const outputBox = document.querySelector("#output");
+outputBox.textContent = `Welcome to table flip! Input a character.`
 
 console.log("hello script js");
 
-var inputHappened = function(currentInput) {
-  for (let i = 0; i < catArray.length; i++) {
-    if (currentInput === catArray[i]) {
-      letterFound = true;
-      numberOfCorrectlyGuessedLetters++;
-      if (letterFound && numberOfCorrectlyGuessedLetters !== catArray.length) {
-        letterFound = false;
-        correctlyGuessedLetters = correctlyGuessedLetters + catArray[i];
-        input.value = "";
-        return `You guessed right! Correctly guessed letters: ${correctlyGuessedLetters}`;
-      } else {
-        return `You won!`;
+const inputHappened = function(currentInput) {
+  for (i = 0; i < words.length; i++) {
+    for (j = 0; j < words[i].length; j++) {
+      if (words[i][j] === currentInput) {
+        letterFound = true;
+        numberOfCorrectlyGuessedLetters++;
+        console.log(numberOfCorrectlyGuessedLetters);
+        if (
+          letterFound &&
+          numberOfCorrectlyGuessedLetters < numberOfArrayLetters
+        ) {
+          letterFound = false;
+          correctlyGuessedLetters = correctlyGuessedLetters + words[i][j];
+          input.value = "";
+          return `You guessed right! Correctly guessed letters: ${correctlyGuessedLetters}
+        Number of correctly guessed letters: ${numberOfCorrectlyGuessedLetters} Letters left: ${numberOfArrayLetters -
+            numberOfCorrectlyGuessedLetters}`;
+        } else {
+          return `You won! Words were:
+          1) cat
+          2) doggy
+          3) alphabet`;
+        }
       }
     }
   }
   input.value = "";
 
   numberOfWronglyGuessedLetters++;
-  flipTableFigure += flipTableArray.shift();
+  flipTableFigure = flipTableFigure + flipTableArray.shift();
   if (flipTableArray.length > 0) {
     return flipTableFigure;
   } else {
-    return `Sorry! You lost. 
+    return `Sorry! You lost.
               (╯ರ ~ ರ）╯︵ ┻━┻`;
   }
 };
