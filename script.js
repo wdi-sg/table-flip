7e08e1e724acb992a8789c3c24704200dd2737b1
Robert Kolsek <kolsek.income@gmail.com>
2019-12-12 23:25:41 +0800

    Step 6 complete

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 let word = [
-  ['c','a','t'],
-  ['d','o','g','g','y'],
-  ['a','l','p','h','a','b','e','t'],
+  ['c', 'a', 't'],
+  ['d', 'o', 'g', 'g', 'y'],
+  ['a', 'l', 'p', 'h', 'a', 'b', 'e', 't'],
   ["hello"],
   ["bathing"]
 ];
-
+// make words to character arrays
 for (let i = 0; i < word.length; i++) {
   if (word[i].length === 1) {
-    word [i] = word[i][0].split("")
+    word[i] = word[i][0].split("")
   }
 }
+// random secret word out of array
+let secretWord = word[Math.floor(Math.random() * ((word.length - 1) - 0 + 1)) + 0]
 
+let tableFlipFirst = ["┻", "━", "┻"]
+let tableFlipSecond = ["(", "╯", "ರ", " ", "~", " ", "ರ", ")", " "]
+let tableFlipThird = [ "╯", "︵"]
+let fullTableFlip = ["(", "╯", "ರ", " ", "~", " ", "ರ", ")", " ", "╯", "︵", "┻", "━", "┻"]
 
-let secretWord = word[Math.floor(Math.random() * ((word.length - 1) - 0 + 1) ) + 0]
+const tableFlipFirstLength = tableFlipFirst.length
+const tableFlipSecondLength = tableFlipSecond.length
+const tableFlipThirdLength = tableFlipSecond.length
 
-let tableFlip = ["(", "╯", "ರ", " ", "~", " ", "ರ", ")", " ", "╯", "︵", " ", "┻", "━", "┻"]
-const gameLength = tableFlip.length
+const gameLength = fullTableFlip.length
 
 let currentTableFlip = []
 
@@ -34,27 +41,43 @@ var inputHappened = function (currentInput) {
   for (let i = 0; i < secretWord.length; i++) {
     if (currentInput === secretWord[i]) {
 
-        let letterIndexFront = secretWord.indexOf(currentInput)
-        correctGuesses[letterIndexFront] = `${currentInput}`
-        let letterIndexBack = secretWord.lastIndexOf(currentInput)
-        if (letterIndexBack !== letterIndexFront) {
-          correctGuesses[letterIndexBack] = `${currentInput}`
-        }
+      let letterIndexFront = secretWord.indexOf(currentInput)
+      correctGuesses[letterIndexFront] = `${currentInput}`
+      let letterIndexBack = secretWord.lastIndexOf(currentInput)
+      if (letterIndexBack !== letterIndexFront) {
+        correctGuesses[letterIndexBack] = `${currentInput}`
+      }
 
-        correctLetter = correctLetter + currentInput
-        if (correctGuesses.join("") === secretWord.join("")) {
-          return `You win!`
-        }
-        return `You guessed it RIGHT! - Your correct guesses: ${correctGuesses.join("")}`
+      correctLetter = correctLetter + currentInput
+      if (correctGuesses.join("") === secretWord.join("")) {
+        return `You win!`
+      }
+      return `You guessed it RIGHT! - Your correct guesses: ${correctGuesses.join("")}`
     }
   }
-      let shiftedCharacter = tableFlip.shift() 
-      currentTableFlip.push(shiftedCharacter)
-      wrongGuesses++
-      if (wrongGuesses > gameLength) {
-        return `Game over!`
-      } else {
-        return `${currentTableFlip.join("")}`
-      }
-      
+  //continue here, insert second flip other way around! Maybe use FullTableFlip.indexOf?
+  if (wrongGuesses < tableFlipFirstLength) {
+    let shiftedCharacter = tableFlipFirst.shift()
+    currentTableFlip.push(shiftedCharacter)
+    wrongGuesses++
+  } else if (wrongGuesses < tableFlipSecondLength + tableFlipFirstLength) {
+    
+    let shiftedCharacter = tableFlipSecond.pop()
+    currentTableFlip.unshift(shiftedCharacter)
+    wrongGuesses++
+  } else if (wrongGuesses < tableFlipSecondLength + tableFlipFirstLength + tableFlipThirdLength) {
+    let lastIndex = fullTableFlip.lastIndexOf(" ")
+    let shiftedCharacter = tableFlipThird.pop()
+    currentTableFlip.splice(lastIndex, 0, shiftedCharacter)
+    wrongGuesses++
+  }
+  // let shiftedCharacter = tableFlip.shift()
+  // currentTableFlip.push(shiftedCharacter)
+  // wrongGuesses++
+  if (wrongGuesses > gameLength) {
+    return `Game over!`
+  } else {
+    return `${currentTableFlip.join("")}`
+  }
+
 }
