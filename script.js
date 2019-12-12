d51d380c9013b24e11f4c41d0dadd7262529b7c0
Robert Kolsek <kolsek.income@gmail.com>
2019-12-12 19:39:18 +0800

    step 5 done

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let word = [
 
 for (let i = 0; i < word.length; i++) {
   if (word[i].length === 1) {
-    word [i][0] = word[i][0].split("")
+    word [i] = word[i][0].split("")
   }
 }
 
@@ -21,6 +21,11 @@ const gameLength = tableFlip.length
 let currentTableFlip = []
 
 let correctLetter = ""
+let correctGuesses = []
+
+for (let i = 0; i < secretWord.length; i++) {
+  correctGuesses[i] = `_`
+}
 
 let wrongGuesses = 0
 
@@ -28,11 +33,19 @@ var inputHappened = function (currentInput) {
 
   for (let i = 0; i < secretWord.length; i++) {
     if (currentInput === secretWord[i]) {
+
+        let letterIndexFront = secretWord.indexOf(currentInput)
+        correctGuesses[letterIndexFront] = `${currentInput}`
+        let letterIndexBack = secretWord.lastIndexOf(currentInput)
+        if (letterIndexBack !== letterIndexFront) {
+          correctGuesses[letterIndexBack] = `${currentInput}`
+        }
+
         correctLetter = correctLetter + currentInput
-        if (correctLetter.length === secretWord.length) {
+        if (correctGuesses.join("") === secretWord.join("")) {
           return `You win!`
         }
-        return `You guessed it RIGHT! - Your correct guesses: ${correctLetter}`
+        return `You guessed it RIGHT! - Your correct guesses: ${correctGuesses.join("")}`
     }
   }
       let shiftedCharacter = tableFlip.shift() 
