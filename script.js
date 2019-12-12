245199843eb180f43a003076abb93f8577e93cc7
benjacoblee <benjacoblee@gmail.com>
2019-12-12 16:45:54 +0800

    basic logic for cat word

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+let catArray = ["c", "a", "t"];
+let flipTableArray = ["(", "╯", "ರ", "~", "ರ", "）", "╯", "︵", "┻", "━", "┻"];
+let catWord = "";
+let flipTableWord = "";
+let flipIndex;
+
 console.log("hello script js");
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPEND";
+var inputHappened = function(currentInput) {
+  for (let i = 0; i < catArray.length; i++) {
+    if (currentInput === catArray[i]) {
+      catWord = catWord + catArray[i];
+      return catWord;
+    } 
+  }
 };
