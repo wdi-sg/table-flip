2489b0361040717c8c8420e766ca363e18031659
SafrazHakamali <safrazhakamali@gmail.com>
2019-12-12 22:52:13 +0800

    completed further 4: location

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ for (i=0; i<presplitWords.length; i++) {
 
 var whichWord = 0;
 var correctGuess = [];
+var guessing = [];
+for (i=0; i<words[whichWord].length; i++) {
+	guessing.push('_');
+}
 var wrong = true;
 var notify;
 var hangmanAll = ["(","╯","ರ"," ~ ","ರ","）","╯","︵"," ┻","━","┻"];
@@ -21,6 +25,7 @@ var inputHappened = function(currentInput){
   for (i=0; i<words[whichWord].length; i++) {
   	if (currentInput==words[whichWord][i]) {
   		correctGuess.push(words[whichWord][i]);
+  		guessing[i]=currentInput;
   		notify = "You have guessed right.";
   		wrong = false;
   		console.log(wrong);
@@ -36,14 +41,17 @@ var inputHappened = function(currentInput){
   wrong = true;
   document.querySelector('#input').value = ""
   if (correctGuess.length<words[whichWord].length && hangmanTrack.length<hangmanAll.length) {
-  	return notify + " So far you have guessed " + correctGuess.length + " letters correctly. " + correctGuess.join('') +". Table-flip: " + hangmanTrack.join('');
+  	return notify + " So far you have guessed " + correctGuess.length + " letters correctly. " + guessing.join('') +". Table-flip: " + hangmanTrack.join('');
   } else if (correctGuess.length==words[whichWord].length && whichWord<2){
   	whichWord++;
   	correctGuess = [];
-  	console.log(whichWord);
-  	return "You guessed the word! Guess the next word."
+  	guessing = [];
+  	for (i=0; i<words[whichWord].length; i++) {
+	guessing.push('_');
+	}
+	return "You guessed the word "+ words[whichWord-1].join('')+"! Guess the next word."
   } else if (correctGuess.length==words[whichWord].length && whichWord==2){
-  	return "You win! Table-flip: " + hangmanTrack.join('');
+  	return "You won by guessing the last word "+words[whichWord].join('')+"! Table-flip: " + hangmanTrack.join('');
   } else {
   	return "You lose! Table-flipped!: " + hangmanTrack.join('');
   }
