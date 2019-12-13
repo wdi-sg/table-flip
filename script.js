041480152f998d3d5621e49debc76180d68b2e78
SamTanFL <samsite_tan@hotmail.com>
2019-12-13 12:18:54 +0800

    overhauled word object into an array

--- script.js
+++ script.js
@@ -8,37 +8,34 @@ let wordSelection = 0;
 let gameIsOver = false; // help track the state of the game
 let wordAttempts = 0; // track fail progress
 let failFlips = ""; // to help print (╯ರ ~ ರ）╯︵ ┻━┻
-let words = {
-    secretWord1: {
+let words = [ {
         secretArray: ["c", "a", "t"],
         secretWord: ["_", "_", "_"],
         foundLetters: [false, false, false]
-   },
-   secretWord2: {
+   }, {
         secretArray: ["d", "o", "g", "g", "y"],
         secretWord: ["_", "_", "_", "_", "_"],
         foundLetters: [false, false, false, false, false]
-    },
-    secretWord3: {
+    }, {
         secretArray: ["a", "l", "p", "h", "a", "b", "e", "t"],
         secretWord: ["_", "_", "_", "_", "_", "_", "_", "_"],
         foundLetters: [false, false, false, false, false, false, false, false]
     }
-}
+];
 
 
 var letterPosition1 = function(currentInput) {
-    if (currentInput === words.secretWord1.secretArray[0] && words.secretWord1.foundLetters[0] === false) {
-        words.secretWord1.secretWord[0] = words.secretWord1.secretArray[0];
-        words.secretWord1.foundLetters[0] = true;
+    if (currentInput === words[0].secretArray[0] && words[0].foundLetters[0] === false) {
+        words[0].secretWord[0] = words[0].secretArray[0];
+        words[0].foundLetters[0] = true;
         correctLetter++;
-    } else if (currentInput === words.secretWord1.secretArray[1] && words.secretWord1.foundLetters[1] === false) {
-        words.secretWord1.secretWord[1] = words.secretWord1.secretArray[1];
-        words.secretWord1.foundLetters[1] = true;
+    } else if (currentInput === words[0].secretArray[1] && words[0].foundLetters[1] === false) {
+        words[0].secretWord[1] = words[0].secretArray[1];
+        words[0].foundLetters[1] = true;
         correctLetter++;
-    } else if (currentInput === words.secretWord1.secretArray[2] && words.secretWord1.foundLetters[2] === false) {
-        words.secretWord1.secretWord[2] = words.secretWord1.secretArray[2];
-        words.secretWord1.foundLetters[2] = true;
+    } else if (currentInput === words[0].secretArray[2] && words[0].foundLetters[2] === false) {
+        words[0].secretWord[2] = words[0].secretArray[2];
+        words[0].foundLetters[2] = true;
         correctLetter++;} else {
             return;
         }
@@ -46,23 +43,23 @@ var letterPosition1 = function(currentInput) {
 
 
 var letterPosition2 = function(currentInput) {
-    if (currentInput === words.secretWord2.secretArray[0] && words.secretWord2.foundLetters[0] === false) {
-        words.secretWord2.secretWord[0] = words.secretWord1.secretArray[0];
-        words.secretWord2.foundLetters[0] = true;
+    if (currentInput === words[1].secretArray[0] && words[1].foundLetters[0] === false) {
+        words[1].secretWord[0] = words[0].secretArray[0];
+        words[1].foundLetters[0] = true;
         correctLetter++;
-    } else if (currentInput === words.secretWord2.secretArray[1] && words.secretWord2.foundLetters[1] === false) {
-        words.secretWord2.secretWord[1] = words.secretWord1.secretArray[1];
-        words.secretWord2.foundLetters[1] = true;
+    } else if (currentInput === words[1].secretArray[1] && words[1].foundLetters[1] === false) {
+        words[1].secretWord[1] = words[0].secretArray[1];
+        words[1].foundLetters[1] = true;
         correctLetter++;
-    } else if (currentInput === words.secretWord2.secretArray[2] && words.secretWord2.foundLetters[2] === false) {
-        words.secretWord2.secretWord[2] = words.secretWord1.secretArray[2];
-        words.secretWord2.foundLetters[2] = true;
-        correctLetter++;} else if (currentInput === words.secretWord2.secretArray[3] && words.secretWord2.foundLetters[3] === false) {
-        words.secretWord2.secretWord[3] = words.secretWord1.secretArray[3];
-        words.secretWord2.foundLetters[3] = true;
-        correctLetter++;} else if (currentInput === words.secretWord2.secretArray[4] && words.secretWord2.foundLetters[4] === false) {
-        words.secretWord2.secretWord[4] = words.secretWord1.secretArray[4];
-        words.secretWord2.foundLetters[4] = true;
+    } else if (currentInput === words[1].secretArray[2] && words[1].foundLetters[2] === false) {
+        words[1].secretWord[2] = words[0].secretArray[2];
+        words[1].foundLetters[2] = true;
+        correctLetter++;} else if (currentInput === words[1].secretArray[3] && words[1].foundLetters[3] === false) {
+        words[1].secretWord[3] = words[0].secretArray[3];
+        words[1].foundLetters[3] = true;
+        correctLetter++;} else if (currentInput === words[1].secretArray[4] && words[1].foundLetters[4] === false) {
+        words[1].secretWord[4] = words[0].secretArray[4];
+        words[1].foundLetters[4] = true;
         correctLetter++;} else {
             return;
         }
@@ -90,8 +87,8 @@ var gameOver = function() {
 
 var letterSearch = function(currentInput) {
     var i = 0;
-    while( i<words.secretWord1.secretArray.length ){
-        if( words.secretWord1.secretArray[i] === currentInput.toLowerCase()){
+    while( i<words[0].secretArray.length ){
+        if( words[0].secretArray[i] === currentInput.toLowerCase()){
             letterPosition1(currentInput.toLowerCase());
             return;}
         i++}
@@ -109,14 +106,14 @@ var letterSearch = function(currentInput) {
 var inputHappened = function(currentInput){
     if (currentInput.length > 1) {
         document.querySelector('#input').value = ""
-        return "Please input one letter at a time" + "\n" + words.secretWord1.secretWord + "\n" + failFlips + "\n" + "You have " + (14 - wordAttempts) + " attempts left.";
+        return "Please input one letter at a time" + "\n" + words[0].secretWord + "\n" + failFlips + "\n" + "You have " + (14 - wordAttempts) + " attempts left.";
     } else if (currentInput.match(/[a-z]/i) === null) {
                 document.querySelector('#input').value = ""
-                return "Please input a valid character" + "\n" + words.secretWord1.secretWord + "\n" + failFlips + "\n" + "You have " + (14 - wordAttempts) + " attempts left.";
+                return "Please input a valid character" + "\n" + words[0].secretWord + "\n" + failFlips + "\n" + "You have " + (14 - wordAttempts) + " attempts left.";
     } else {
         letterSearch(currentInput);
         document.querySelector('#input').value = ""
-        if (correctLetter === words.secretWord1.secretArray.length && gameIsOver === false) {
+        if (correctLetter === words[0].secretArray.length && gameIsOver === false) {
             win(); }
-            return currentInput + "\n" + words.secretWord1.secretWord + "\n" + failFlips + "\n" + "You have " + (14 - wordAttempts) + " attempts left."; }
+            return currentInput + "\n" + words[0].secretWord + "\n" + failFlips + "\n" + "You have " + (14 - wordAttempts) + " attempts left."; }
 };
