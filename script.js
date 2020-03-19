29ba72ce9c3b6f3ca913718959a68dee8d94e896
ThomasOh92 <ohcsthomas@gmail.com>
2020-03-19 23:52:27 +0800

    Made sure that you cannot guess the same letter twice

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.querySelector('#output').innerText = "Please pick a letter";
 var letterCheck = function(letter){
     if (secretWord.includes(letter)){
         return true;
-    } else {
+    } else  {
         return false;
     }
 }
@@ -32,30 +32,32 @@ var inputHappened = function(currentInput){
 
 //if letter is correct, or not
     if (letterIsCorrect){
-    correctlyGuessedLetters.push(currentInput);
+        correctlyGuessedLetters.push(currentInput);
+        secretWord = secretWord.filter(letter => letter !== currentInput);
     } else {
-    currentTableFlip.push(tableFlip[wronglyGuessedLetters])
-    wronglyGuessedLetters++;
+        currentTableFlip.push(tableFlip[wronglyGuessedLetters])
+        wronglyGuessedLetters++;
     }
 
  //if letter is correct, but game is not won
-    if (letterIsCorrect && correctlyGuessedLetters.length < secretWord.length){
-    output = `You guessed right! These are the letters you have guessed right so far \n ${correctlyGuessedLetters.join(' ')}`;
+    if (letterIsCorrect && secretWord.length > 0){
+        output = `You guessed right! These are the letters you have guessed right so far \n ${correctlyGuessedLetters.join(' ')}`;
     }
 //if letter is correct, and game is won
-    else if (letterIsCorrect && correctlyGuessedLetters.length === secretWord.length){
-    output = `You have won! Congratulations! The secret word was: \n\n ${secretWord.join('')}`;
+    else if (letterIsCorrect && secretWord.length === 0){
+        output = `You have won! Congratulations! The secret word was.... \n\n cat!`;
     }
 //if letter is wrong, but game is not lost
     else if (!letterIsCorrect && currentTableFlip.length < tableFlip.length){
-    output = currentTableFlip.join('');
+        output = currentTableFlip.join('');
     }
 //if letter is wrong, and game is lost
     else if (!letterIsCorrect && currentTableFlip.length === tableFlip.length) {
-    output = `${currentTableFlip.join('')} \n Game Over!`
+        output = `${currentTableFlip.join('')} \n Game Over!`
     }
+//error catch
     else {
-    output = "error in main function"
+        output = "error in main function"
     }
 
     clearInput();
