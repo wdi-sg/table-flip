93d45d51433b2d361ec573971463041f6480c325
wickker <choydianchun@gmail.com>
2020-03-19 22:03:40 +0800

    completed compulsory section

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-console.log("hello script js");
+let secretWordCopy = ["c", "a", "t"];
+let secretWord = ["c", "a", "t"];
+let wrongGuess = 0;
+let rightGuess = 0;
+let tableFlipArray = [
+  "(",
+  "╯",
+  "ರ ",
+  "~",
+  " ರ",
+  "）",
+  "╯",
+  "︵",
+  "┻",
+  "━",
+  "┻"
+];
+let outputRightArray = [];
+let outputWrongArray = [];
+let outputMsg;
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPEND";
+var inputHappened = function(currentInput) {
+  for (let i = 0; i < secretWord.length; i++) {
+    if (secretWord[i] === String(currentInput)) {
+      rightGuess++;
+      outputRightArray.push(String(currentInput));
+      secretWord.splice(i, 1);
+      if (rightGuess === secretWordCopy.length) {
+        outputMsg =
+          "You have won! The secret word is: " +
+          secretWordCopy.join('') +
+          ". Refresh page to continue playing.";
+        return outputMsg;
+      } else {
+        outputMsg =
+          "You guessed right! \n Correctly guessed: " + outputRightArray + ".";
+        return outputMsg;
+      }
+    }
+  }
+  wrongGuess++;
+  outputWrongArray.push(tableFlipArray[wrongGuess - 1]);
+  if (wrongGuess === tableFlipArray.length) {
+    outputMsg = outputWrongArray.join('') + "You have lost! Refresh page to play again.";
+    return outputMsg;
+  } else {
+    outputMsg =
+      "You guessed wrong. \n You are " +
+      (tableFlipArray.length - wrongGuess) +
+      " lives closer to a table flip: " +
+      outputWrongArray.join('') +
+      ".";
+    return outputMsg;
+  }
 };
