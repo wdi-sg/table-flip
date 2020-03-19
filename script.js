1bd17875192bb78ee8986aa8d20c409c84ae8f15
Zachariah Chow <zachariahchow@gmail.com>
2020-03-20 04:15:30 +0800

    fixed bug that prevented user from winning

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 document.getElementById("input").setAttribute("maxlength", "1");
 
-var secretWords = [['c', 'a', 't'], ['d', 'o', 'g', 'g', 'y'], ['r', 'e', 'm', 'o', 't', 'e', 'e', 'r']];
-var secretWord = secretWords[2];
+var generateSecretWord = (word) => {
+    secretWords.push(word.split(""));
+};
+
+var secretWords = [['c', 'a', 't'], ['d', 'o', 'g', 'g', 'y'], ['r', 'e', 'm', 'o', 't', 'e'], ['n', 'i', 'n', 'c', 'o', 'm', 'p', 'o', 'o', 'p'], ['p', 'o', 'p', 'p', 'y', 'c', 'o', 'c', 'k']];
+
+var secretWord = secretWords[(Math.floor(Math.random() * secretWords.length))];
 
 var tableFlip = ['(', '╯', 'ರ', '~', 'ರ', ')', '╯', '︵', '┻', '━', '┻'];
 var guessValue;
+var refresh = true;
 var gameEnd;
 var correctlyGuessed = [];
 var wronglyGuessed = [];
@@ -39,18 +45,20 @@ var inputHappened = function(currentInput){
                 arr2.push(tableFlipBackup[i]);
             }
 
-            gameEnd = false;
+            // gameEnd = false;
             correctlyGuessed.length = 0;
             wronglyGuessed.length = 0;
-            correctGuessDisplay.length = 0;
+            secretWordBackup.length = 0
+            correctGuessDisplay.length = 0
         }
 
     var gameHandler = arr => {
         if (guessValue) {
             correctlyGuessed.push(guessValue);
             var index = arr.indexOf(guessValue);
-            arr.splice(index, 1);
-            //
+            // arr.splice(index, 1);
+            // console.log(correctlyGuessed);
+
             var correctGuessIndex = [];
             for (var i = 0; i < secretWordBackup.length; i++) {
                 if (secretWordBackup[i] === guessValue) {
@@ -63,45 +71,46 @@ var inputHappened = function(currentInput){
                 return correctGuessDisplay[element] = guessValue;
             })
 
-            if (arr.length === 0){
+            if (correctGuessDisplay.indexOf("_") === -1) {
                 gameEnd = true;
                 resetUponWinLose(secretWord, tableFlip);
                 return `Congratulations! You've guessed the secret word ${secretWordBackup.join('')}!`;
             } else {
-                return `Good guess!`;
+                return `Good guess!` +
+                `\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `;
             }
+
         } else if (tableFlip.length > 1) {
             tableFlip.length--;
             wronglyGuessed.push(currentInput.toLowerCase());
-            return `┳━┳\nWrong. Guess again. ${tableFlip.length} guesses left.`
+            return `┳━┳\nWrong. Guess again. ${tableFlip.length} guesses left.` +
+            `\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `;
+
         } else if (tableFlip.length === 1) {
             tableFlip.length--;
             wronglyGuessed.push(currentInput.toLowerCase());
-            return `(ರ ~ ರ）┳━┳\nFinal guess. `
+            return `(ರ ~ ರ）┳━┳\nFinal guess. ` + `\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `;
+
         } else {
             wronglyGuessed.push(currentInput.toLowerCase());
             gameEnd = true;
             resetUponWinLose(secretWord, tableFlip);
-            return `(╯ರ ~ ರ）╯︵ ┻━┻\nOut of guesses, you suck at this. `
+            return `(╯ರ ~ ರ）╯︵ ┻━┻\nOut of guesses, you suck at this. ` + `\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `;
         }
     }
     //Validate User Input
-    if (gameEnd === true) {
-        return "";
-
+    if (gameEnd === true || refresh === true) {
+        gameEnd = false;
+        refresh = false;
+        return `Guess the word: ${correctGuessDisplay.join(' ')}`;
     } else if (regex.test(currentInput) === false) {
-        return "Please enter only alphabets. " +
-        `\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `;
+        return `Please enter only alphabets.\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `
 
-    } else if ((correctlyGuessed.includes(currentInput.toLowerCase()) ||
-        wronglyGuessed.includes(currentInput.toLowerCase())) &&
-        secretWord.includes(currentInput) === false) {
-        return "You have made this guess before." +
-        `\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `;
-
+    } else if (correctGuessDisplay.includes(currentInput.toLowerCase()) ||
+        wronglyGuessed.includes(currentInput.toLowerCase())) {
+        return "You have made this guess before." + `\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `
     } else {
         checkInput(secretWord);
-        return gameHandler(secretWord) +
-        `\nCorrect Guesses: ${correctGuessDisplay.join(' ')}\nWrong Guesses: ${wronglyGuessed.join(' ')} `;
+        return gameHandler(secretWord)
     }
 };
