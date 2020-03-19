d8ad36e5a2481cc22999de6503c002c60865f0e1
travisenquiry <travisenquiry@gmail.com>
2020-03-19 22:13:12 +0800

    completed part 1

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 console.log("hello script js");
 
+var secretWord = ['c', 'a', 't'];
+var tableFlip = ['(', '╯', 'ರ',  '~',  'ರ', '╯', '︵',  '┻', '━', '┻'];
+var rightCounter = 0;
+var wrongCounter = 0;
+var currentTableFlip = [];
+var guessedRightLetters = [];
+var tableFlipLength = tableFlip.length;
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPEND";
+    for(var i=0; i < secretWord.length; i++){
+        if(secretWord[i] === currentInput){
+            guessedRightLetters.push(currentInput);
+            rightCounter = rightCounter + 1;
+            return `you guessed right: ${guessedRightLetters}`;
+        }
+    }
+
+    for(var i=0; i < secretWord.length; i++){
+        if(secretWord[i] !== currentInput && wrongCounter < tableFlipLength){
+            wrongCounter = wrongCounter + 1;
+            currentTableFlip.push(tableFlip.shift());
+            return currentTableFlip;
+        }
+    }
+    return "Game is over";
 };
