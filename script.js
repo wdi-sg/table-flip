6f731792bdf659986c07ebf45c13949dcfb18c29
kokchuantan <kokchuan1996@gmail.com>
2020-03-20 00:29:28 +0800

    tried my best for part 2 really dodgy working version

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 console.log("hello script js");
 
-var secretWord = ['c','a','t'];
+var secretWord = [  ['c','a','t'] , ['d','o','g','g','y'], ['a','l','p','h','a','b','e','t'] ];
 var flip = [ '(' , '╯' , 'ರ' , '~' , 'ರ' , '）' , '╯' , '︵' , '┻' , '━' ];
 var correctWord=[];
 var wrongWord='';
 var guess;
-var x;
+var x, y;
 var control = false;
 var temp;
+var totalLength = secretWord[0].length + secretWord[1].length + secretWord[2].length;
+console.log(totalLength);
 
 document.querySelector('#output').innerText ='Start guessing!';
 
@@ -15,15 +17,21 @@ var inputHappened = function(currentInput){
     console.log( currentInput );
     if(secretWord){
         if(flip != 0){
-            if(secretWord != 0){
+            if (correctWord.length == 16){
+                    return 'Congratulations! You found them all! \n The letters you got were ' + correctWord + '. \n The words were cat, doggy and alphabet !';
+                }
+            else if(secretWord != 0){
                 for (x = 0; x < secretWord.length ; x++){
-                    console.log(secretWord.length);
-                    if(currentInput == secretWord[x]){
-                        correctWord = correctWord + secretWord[x];
-                        secretWord.splice(x,1);
-                        console.log(correctWord);
-                        console.log(secretWord);
-                        return "Congratulations! You found " + correctWord ;
+                    for(y = 0; y < secretWord[x].length ; y++){
+                        if(currentInput == secretWord[x][y]){
+                            correctWord = correctWord + secretWord[x][y];
+                            //secretWord.splice([x][y],1);
+                            delete secretWord[x][y];
+                            secretWord.filter(Boolean);
+                            console.log(correctWord);
+                            console.log(secretWord);
+                            return "Congratulations! You found " + correctWord ;
+                        }
                     }
                 }
                 if(currentInput != secretWord[x]){
@@ -33,9 +41,6 @@ var inputHappened = function(currentInput){
                         return wrongWord;
                     }
             }
-            else if (secretWord == 0){
-                    return 'Congratulations! You found them all! The word is ' + correctWord + '!';
-                }
         }
         else{
             return 'You have run out of tries!';
