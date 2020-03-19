6380002e40dbf380fbd41c50bed72f48ac93ff37
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-19 23:49:10 +0800

    Nested loops to search through array in array.
    return statement for incorrect letters must be placed outside for loop or return statement will prematurely break for loop.

--- script.js
+++ script.js
@@ -1,7 +1,82 @@
 console.log("hello script js");
+////////////////////////////Part 1
+// var secretWord = ['c', 'a', 't'];
+// var corretLetters = []
+// var flipTable = ['(', '╯', 'ರ', ' ~', ' ರ', '）', '╯', '︵', ' ┻', '━', '┻'];
+// var currentTable = []
+// var wrongGuesses = 0;
 
-var secretWord = ['c', 'a', 't'];
-var corretLetters = []
+// var initialize = function(){
+//     document.getElementById('input').value = "";
+// };
+
+
+// var inputHappened = function(currentInput){
+//   console.log( currentInput );
+
+//   //Create a loop to loop secretWord with input
+//   for (var i=0; i<secretWord.length; i++){
+
+//     //if letter correct
+//     if(secretWord[i] === currentInput){
+
+//         //add letter to array and print array
+//         corretLetters.push(currentInput);
+
+//         //check if player won
+//         if (corretLetters.length === secretWord.length) {
+//             initialize();
+//             return `You WON!!! the word is CAT`
+//         }
+
+//         else{
+//         // print message
+//             initialize();
+//             return `you guessed right! your correct letters are ${corretLetters}`
+//         }
+
+//     }
+
+//     //if letter wrong
+//     else{
+
+//         // remove flipTable character and add to currentTable
+//         currentTable.push(flipTable.shift());
+
+//         //Keep track of number of wrong guesses
+//         wrongGuesses++;
+
+//         //if whole flip table, message game is over.
+//         if (flipTable.length === 0) {
+//             return `(╯ರ ~ ರ）╯︵ ┻━┻ YOU LOST!!!` ;
+//         }
+
+//         else {
+//             initialize();
+//             return `${currentTable}, you have made ${wrongGuesses} wrong guesses.`
+//         }
+//     }
+//   }
+// };
+
+
+
+
+////////////////////////////further 1
+var secretWord = [
+  ['c','a','t'],
+  ['d','o','g','g','y'],
+  ['a','l','p','h','a','b','e','t']
+];
+
+var totalLetters = 0;
+for (var i=0; i<secretWord.length; i++){
+    for (var e=0; e<secretWord[i].length; e++){
+        totalLetters++;
+    }
+}
+
+var correctLetters = []
 var flipTable = ['(', '╯', 'ರ', ' ~', ' ರ', '）', '╯', '︵', ' ┻', '━', '┻'];
 var currentTable = []
 var wrongGuesses = 0;
@@ -12,49 +87,58 @@ var initialize = function(){
 
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
+  // console.log( currentInput );
 
-  //Create a loop to loop secretWord with input
-  for (var i=0; i<secretWord.length; i++){
+    //Create a loop in secretWord and loop again over the array to check for correct letter
+    for (var i=0; i<secretWord.length; i++){
 
-    //if letter correct
-    if(secretWord[i] === currentInput){
+        for (var e=0; e<secretWord[i].length; e++){
 
-        //add letter to array and print array
-        corretLetters.push(currentInput);
+            console.log(secretWord[i][e]);
 
-        //check if player won
-        if (corretLetters.length === secretWord.length) {
-            initialize();
-            return `You WON!!! the word is CAT`
-        }
+            //if letter correct
+            if(secretWord[i][e] === currentInput){
 
-        else{
-        // print message
-            initialize();
-            return `you guessed right! your correct letters are ${corretLetters}`
-        }
+                //remove letter from secretWord
+                secretWord[i].splice(e, 1);
+                console.log(secretWord);
 
-    }
+                //add letter to array and print array
+                correctLetters.push(currentInput);
 
-    //if letter wrong
-    else{
+                //check if player won
+                if (correctLetters.length === totalLetters) {
+                    initialize();
+                    return `You WON!!! the word is CAT, DOGGY and ALPHABET`
+                }
 
-        // remove flipTable character and add to currentTable
-        currentTable.push(flipTable.shift());
+                else{
+                // print message
+                    initialize();
+                    return `you guessed right! your correct letters are ${correctLetters}`
+                }
 
-        //Keep track of number of wrong guesses
-        wrongGuesses++;
+            }
 
-        //if whole flip table, message game is over.
-        if (flipTable.length === 0) {
-            return `(╯ರ ~ ರ）╯︵ ┻━┻ YOU LOST!!!` ;
-        }
 
-        else {
-            initialize();
-            return `${currentTable}, you have made ${wrongGuesses} wrong guesses.`
         }
     }
-  }
+
+    //if no return executed, letter is wrong
+    // remove flipTable character and add to currentTable
+    currentTable.push(flipTable.shift());
+
+    //Keep track of number of wrong guesses
+    wrongGuesses++;
+
+    //if whole flip table, message game is over.
+    if (flipTable.length === 0) {
+        return `(╯ರ ~ ರ）╯︵ ┻━┻ YOU LOST!!!` ;
+    }
+
+    else {
+        initialize();
+        return `${currentTable}, you have made ${wrongGuesses} wrong guesses.`
+    }
+
 };
