8dbc55a1b604bb9bf6c95a784697055eb1b59b09
BOBBY <BOBBYKWONGZW@GMAIL.COM>
2020-03-19 22:54:02 +0800

    first commit. Simple correct and wrong guesses with wins and losses measured.

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 console.log("hello script js");
 
+var secretWord = ['c', 'a', 't'];
+var corretLetters = []
+var flipTable = ['(', '╯', 'ರ', ' ~', ' ರ', '）', '╯', '︵', ' ┻', '━', '┻'];
+var currentTable = []
+var wrongGuesses = 0;
+
+var initialize = function(){
+    document.getElementById('input').value = "";
+};
+
+
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPEND";
+
+  //Create a loop to loop secretWord with input
+  for (var i=0; i<secretWord.length; i++){
+
+    //if letter correct
+    if(secretWord[i] === currentInput){
+
+        //add letter to array and print array
+        corretLetters.push(currentInput);
+
+        //check if player won
+        if (corretLetters.length === secretWord.length) {
+            initialize();
+            return `You WON!!! the word is CAT`
+        }
+
+        else{
+        // print message
+            initialize();
+            return `you guessed right! your correct letters are ${corretLetters}`
+        }
+
+    }
+
+    //if letter wrong
+    else{
+
+        // remove flipTable character and add to currentTable
+        currentTable.push(flipTable.shift());
+
+        //Keep track of number of wrong guesses
+        wrongGuesses++;
+
+        //if whole flip table, message game is over.
+        if (flipTable.length === 0) {
+            return `(╯ರ ~ ರ）╯︵ ┻━┻ YOU LOST!!!` ;
+        }
+
+        else {
+            initialize();
+            return `${currentTable}, you have made ${wrongGuesses} wrong guesses.`
+        }
+    }
+  }
 };
