574283fa61a60ccf8aa188557ab8ac625ab329be
elisaes <alisa260785@gmail.com>
2020-03-20 13:40:36 +0800

    corrected

--- script.js
+++ script.js
@@ -21,19 +21,21 @@ let findLetter = function(array, letterUser) {
   for (i = 0; i < array.length; i++) {
     console.log(array[i] === letterUser);
     if (array[i] === letterUser) {
-      correctLetters.push();
-     correctAnswer(letterUser);
+      correctLetters.push(letterUser);
+      return correctAnswer(letterUser);
     }
-    wrongLetters.push();
-    wrongAnswer(letterUser);
   }
+
+  wrongLetters.push(letterUser);
+  return wrongAnswer(letterUser);
 };
 
 var inputHappened = function(currentInput) {
+  document.getElementById("input").value = "";
   if (wrongLetters.length < wrongAnswerCaracter.length) {
-    // if ((correctLetters.length === secretWord.length)) {
-    //   return "You Have won" + secretWord;
-    // }
+    if (correctLetters.length === secretWord.length) {
+      return "You Have won" + secretWord;
+    }
     return findLetter(secretWord, currentInput);
   } else {
     "sorry, you lost" + wrongAnswerCaracter;
