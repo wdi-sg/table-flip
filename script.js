701dacbe03db72ec35e4557ff2afc001d13a983a
Kenan <xinkenan.11@hotmail.com>
2020-03-20 10:24:05 +0800

    :sparkles: part 5 half way working

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const SECRET_WORDS = [
-  ['c', 'a', 't'],
-  ['d', 'o', 'g', 'g', 'y'],
-  ['a', 'l', 'p', 'h', 'a', 'b', 'e', 't']
-];
+
+let secretWordsStr = "cat,doggy,alphabet";
+let secretWordsArr = secretWordsStr.split(',');
+const SECRET_WORDS = secretWordsArr.map(word => word.split(''));
+
 let tableFlipCharacters = "(╯ರ ~ ರ）╯︵ ┻━┻".split(' ');
 let numGuessChancesLeft = tableFlipCharacters.length;
 let correctGuesses = [];
 let flippedCharacters = [];
+let gameBoard = [];
+let gameBoardStr = "";
+let round = 0;
 
 
 const sanitizeInput = input => input.trim();
@@ -16,30 +19,57 @@ const isUserWon = (guessedWords, secretWords) => correctGuesses.length === secre
   .reduce((totalLength, word) => totalLength = totalLength + word.length, 0);
 
 const getCurrentGuessChar = userGuess => userGuess.charAt(userGuess.length - 1);
-const madeAcorrectGuess = (userGuess, secretWords) => secretWords.some(word => word.includes(userGuess))
+const madeAcorrectGuess = (userGuess, secretWords) => secretWords.some(word => word.includes(userGuess));
+/* === long version ===
+* function makeAcorrectGuess (userGuess, secretWords) {
+*   for (let i = 0; i < secretWords.length; i++ ) {
+*     let word = secretWords[i]
+*     for (let j = 0; j <  word.length; j++) {
+*       let letter = word[j];
+*       if (userGuess === letter) return true
+*     }
+*   }
+* */
 const isWrongGuess = (userGuess, secretWords) => !madeAcorrectGuess(userGuess, secretWords);
+
+// displays letter place holders at start of game
+const formatGameBoard = gameBoard => gameBoard.join("<br>");
+const initGameBoard = secretWordsArr => {
+  gameBoard = secretWordsArr.map(word => "_".repeat(word.length) );
+  gameBoardStr = formatGameBoard(gameBoard);
+  document.querySelector('#output').innerHTML= gameBoardStr;
+};
+
+// update gamebaord to replace correct guesses with the right letter
+const updateGameBoard = (correctGuesses, gameBoard) => {
+  console.log(gameBoard);
+  console.log(correctGuesses);
+  console.log(gameBoardStr)
+};
+
+initGameBoard(secretWordsArr);
 
 const inputHappened = function (currentInput) {
   // assumptions:
-  // - user types in one letter at a time | guess character is always last letter of input
+  // - user types one letter at a time | guess character is always last letter of input
   // - user does not press backspace key to delete a typed character
   // - letters in secret word do not repeat
-
   let userInput = sanitizeInput(currentInput);
   let userGuess = getCurrentGuessChar(userInput);
-
-  if (isGameOver(numGuessChancesLeft)) return "Game Over!";
-  if (isUserWon(correctGuesses, SECRET_WORDS)) return "You Won!";
+  if (isGameOver(numGuessChancesLeft)) return "Game Over!\n" + gameBoardStr;
+  if (isUserWon(correctGuesses, SECRET_WORDS)) return "You Won!\n" + gameBoardStr;
 
   if (madeAcorrectGuess(userGuess, SECRET_WORDS)) {
     correctGuesses.push(userGuess);
-    return "You made a correct guess";
+    updateGameBoard(correctGuesses, gameBoard);
+    return `You made a correct guess\n ${flippedCharacters.join('')} \n ${gameBoardStr}`;
   }
 
   if (isWrongGuess(userGuess, SECRET_WORDS)) {
     flippedCharacters.push(tableFlipCharacters.pop());
     numGuessChancesLeft--;
-    return `You made a wrong guess. \n ${flippedCharacters.join('')}`;
+    updateGameBoard(gameBoard, userGuess);
+    return `You made a wrong guess. \n ${flippedCharacters.join('')} \n ${gameBoardStr}`;
   }
 };
 
