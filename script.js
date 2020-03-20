50441dd8f47b17e993d663da3f4850ef79b562f0
Dyana Wu <dyanawu@gmail.com>
2020-03-20 12:53:28 +0800

    [Complete basic] Implement lose condition and state

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //setup for later
 var ikea = "(╯ರ ~ ರ）╯︵ ┻━┻";
 var tableArr = ikea.split("");
+var flippedTable = [];
 
 var secretWord = "data";
 var secretArray = secretWord.split("");
@@ -9,16 +10,22 @@ var guessedCount = 0;
 var guessedArray = [];
 
 var inputHappened = function(currentInput){
-  console.log( currentInput );
   clearInput();
 
   var guessResult = `${currentInput} is ${checkGuess(currentInput)[0]}\n` +
       `You've tried these letters so far: ${guessedArray.join(" ")}\n`;
   var guessesLeft = `${secretWord.length - guessedCount} letters left to guess!`;
-  var winner = `That's it, you guessed the word - "${secretWord}"`;
+  var loseStr = `Sorry, you've run out of tries!\n${flippedTable.reverse().join("")}`;
+  var winStr = `That's it, you guessed the word - "${secretWord}"`;
+
+  console.log(flippedTable.reverse().join(""));
 
   if (guessedCount === secretWord.length) {
-    return guessResult + winner;
+    return guessResult + winStr;
+  }
+
+  if (tableArr.length === 0) {
+    return guessResult + loseStr;
   }
   return guessResult + guessesLeft;
 };
@@ -36,18 +43,18 @@ var checkGuess = function (letter) {
   }
 
   guessedArray.push(letter);
-  
-  for (var i = 0; i < secretWord.length; i++) {
-    console.log(guessedArray, guessedCount, guessed);
 
+  for (var i = 0; i < secretWord.length; i++) {
     if (secretArray[i] === letter) {
       guessed = true;
       guessedCount++;
     }
+  }
 
+  if (!guessed) {
+    flippedTable.push(tableArr.pop());
   }
 
   var guessString = guessed ? "a match!" : "not a match.";
-
-  return [guessed, guessedCount];
+  return [guessString, guessedCount];
 }
