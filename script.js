53b1e73843a6328fec10f84afcbeb3256f7dab23
Rachelik <rachel.ik.87@gmail.com>
2020-03-20 09:26:18 +0800

    Part 1 (incomplete yet)

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 console.log("hello script js");
 
+var secretWord = ["c", "a", "t"];
+
+var tableFlip = ["(", "╯", "ರ", " ~ ", "ರ", "）", "╯", "︵", "┻", "━", "┻"];
+
+var wrongGuess = 0;
+var correctGuess = [];
+
+
+
+
+function userGuess(letter) {
+    if (isNaN(letter)) {
+        for (var j = 0; j < tableFlip.length; j++) {
+            for (var i = 0; i < secretWord.length; i++) {
+                if (letter === secretWord[i]) {
+                    console.log("correct guess: " + letter);
+                    correctGuess.push(letter);
+                    secretWord.splice(i, 1);
+                    return "correct guess: " + letter;
+                };
+            };
+        };
+    } else {
+            console.log("Is not a letter.");
+    };
+    console.log(secretWord);
+};
+
+
 var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPEND";
+  output = userGuess(currentInput);
+  return output;
 };
