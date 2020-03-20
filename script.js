ef8589d050a90ecce17af19a84b65ff81e5bd571
mdazharaw <darkaz62@hotmail.com>
2020-03-20 11:26:58 +0800

    Part 2 done.

--- script.js
+++ script.js
@@ -21,46 +21,68 @@ inputPrompt.placeholder = "Guess a letter";
 function clear() {
     inputPrompt.value = "";
 }
-function wordBuilder(array){
-    var word ="";
-    for (element in array){
+
+function validInput(char){
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    if(alphabet.includes(char)){
+    }
+    else{
+        alert("Invalid input, only single lower case letters instead");
+
+    }
+}
+function wordBuilder(array) {
+    var word = "";
+    for (element in array) {
         word = word + array[element];
     }
     return word;
 }
-function partialWordBuilder(array, index){
-    var word ="";
-    for (i=0; i<=index; i++){
-        word = word + array[i];
-    }
-    return word;
-}
-function uniquePush(char, array){
-    if (!array.includes(char)){
-        array.push(char);
+
+function partialWordBuilder(array, index) {
+    var word = "";
+    if (index <= array.length - 1) {
+        for (i = 0; i <= index; i++) {
+            word = word + array[i];
+        }
+        return word;
     }
     else{
+        return wordBuilder(array);
+    }
+
+
+}
+
+
+
+function uniquePush(char, array) {
+    if (!array.includes(char)) {
+        array.push(char);
+    } else {
         alert(`You already guessed "${char}"`);
     }
 }
 // Looks for character in given array, updates global variable for winning condition tracking
 function searchArray(enteredChar, array) {
-    var result=false;
+    var result = false;
     if (array.includes(enteredChar)) {
         uniquePush(enteredChar, correctGuesses);
         console.log(`good guess ${correctGuesses}`);
         result = true;
-        var reactEmoji = partialWordBuilder(resultChar, (incorrectCounter-1));
+        var reactEmoji = partialWordBuilder(resultChar, (incorrectCounter - 1));
         outputMsg = (`${enteredChar} is correct.\nCorrect guesses so far: ${correctGuesses}\n Wrong guesses so far: ${wrongGuesses}\n${reactEmoji}`);
     } else {
         uniquePush(enteredChar, wrongGuesses);
         console.log(`wrong guess ${wrongGuesses}`);
         incorrectCounter = incorrectCounter + 1;
         if (incorrectCounter <= resultChar.length) {
-            var reactEmoji = partialWordBuilder(resultChar, (incorrectCounter-1));
+            var reactEmoji = partialWordBuilder(resultChar, (incorrectCounter - 1));
             outputMsg = (`${enteredChar} is wrong.\nCorrect guesses so far: ${correctGuesses}\n Wrong guesses so far: ${wrongGuesses}\n${reactEmoji}`);
         } else if (incorrectCounter > resultChar.length) {
-            outputMsg = "Game Over!";
+            var reactEmoji = partialWordBuilder(resultChar, (incorrectCounter - 1));
+            outputMsg = (`${enteredChar} is wrong.\nCorrect guesses so far: ${correctGuesses}\n Wrong guesses so far: ${wrongGuesses}\n${reactEmoji}`);
+            outputMsg += "\n\nGame Over!";
         }
 
     }
@@ -69,7 +91,6 @@ function searchArray(enteredChar, array) {
 
 // Takes the guess and prints message according to current score
 function makeAGuess(character) {
-
     var searchResult = searchArray(character, secretWord);
     var printResult = "";
 
@@ -78,7 +99,11 @@ function makeAGuess(character) {
         return printResult;
     } else {
         var word = wordBuilder(secretWord);
-        printResult = (`You Win!\nThe secret word was "${word}."`);
+        var reactEmoji = partialWordBuilder(resultChar, (incorrectCounter - 1));
+        outputMsg = (`${character} is correct.\nCorrect guesses so far: ${correctGuesses}\n Wrong guesses so far: ${wrongGuesses}\n${reactEmoji}`);
+        outputMsg += (`\n\nYou Win!\nThe secret word was "${word}."`);
+        printResult = outputMsg;
+        //printResult = (`You Win!\nThe secret word was "${word}."`);
 
         return printResult;
     }
@@ -88,6 +113,7 @@ function makeAGuess(character) {
 
 var inputHappened = function(currentInput) {
     console.log(currentInput);
+    validInput(currentInput);
     var output = makeAGuess(currentInput);
     clear();
     return output;
