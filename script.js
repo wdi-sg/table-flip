7ae783f605c202053d503147d9a4f0196efbb6c7
elisaes <alisa260785@gmail.com>
2020-03-20 09:12:00 +0800

    first part

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-console.log("hello script js");
+const secretWord = ["c", "a", "t"];
+const wrongAnswerCaracter = ["(╯", "ರ~", "ರ）", "╯︵", "┻━┻"];
 
-var inputHappened = function(currentInput){
-  console.log( currentInput );
-  return "WOW SOMETHING HAPPEND";
+// let valueWronLetters = parseInt(wrongLetters.length)
+
+var inputHappened = function(currentInput) {
+  const correctLetters = [];
+  const wrongLetters = [];
+  let findLetter = function(array, letterUser) {
+    for (i = 0; i < array.length; i++) {
+      if (array[i] === letterUser) {
+        return i;
+             
+      } else {
+        return -1;
+      }
+    }
+  };
+  //     document.getElementById("input").value = "";
+  if (wrongLetters.length <= wrongAnswerCaracter.length) {
+    findLetter(secretWord, currentInput);
+  } else {
+    return "Sorry you lost" + wrongAnswerCaracter;
+  }
+};
+let correctAnswer = function(currentInput) {
+  return (
+    "You have guessed correct, there is a" +
+    " " +
+    currentInput +
+    "in the secret word"
+  );
+};
+let wrongAnswer = function(currentInput) {
+  return (
+    "Sorry, there is no" + " " + currentInput + "in the secret word, try again"
+  );
 };
