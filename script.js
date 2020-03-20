a01df2d407e7dd0555f0ae58739e1bc12ebc7ccf
kokchuantan <kokchuan1996@gmail.com>
2020-03-20 10:30:46 +0800

    fixed part 2, now working fine

--- script.js
+++ script.js
@@ -17,17 +17,16 @@ var inputHappened = function(currentInput){
     console.log( currentInput );
     if(secretWord){
         if(flip != 0){
-            if (correctWord.length == 16){
-                    return 'Congratulations! You found them all! \n The letters you got were ' + correctWord + '. \n The words were cat, doggy and alphabet !';
-                }
-            else if(secretWord != 0){
+            if(totalLength != 0){
                 for (x = 0; x < secretWord.length ; x++){
                     for(y = 0; y < secretWord[x].length ; y++){
                         if(currentInput == secretWord[x][y]){
                             correctWord = correctWord + secretWord[x][y];
-                            //secretWord.splice([x][y],1);
-                            delete secretWord[x][y];
-                            secretWord.filter(Boolean);
+                            secretWord[x].splice(y,1);
+                            totalLength--;
+                            console.log(totalLength);
+                            //delete secretWord[x][y];
+                            //secretWord.filter(Boolean);
                             console.log(correctWord);
                             console.log(secretWord);
                             return "Congratulations! You found " + correctWord ;
@@ -41,6 +40,9 @@ var inputHappened = function(currentInput){
                         return wrongWord;
                     }
             }
+            else if (totalLength == 0){
+                    return 'Congratulations! You found them all! \n The letters you got were ' + correctWord + '. \n The words were cat, doggy and alphabet !';
+                }
         }
         else{
             return 'You have run out of tries!';
