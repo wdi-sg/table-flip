c8dbd621c75f560d899d289499a8f3ac12a15325
Ari <ari@Aris-MacBook-Air.local>
2020-03-20 14:32:32 +0800

    changed the js file

--- script.js
+++ script.js
@@ -2,5 +2,25 @@ console.log("hello script js");
 
 var inputHappened = function(currentInput){
   console.log( currentInput );
-  return "WOW SOMETHING HAPPEND";
+  //return "WOW SOMETHING HAPPEND";
 };
+
+var secretWord = ['c','a','t'];
+var userAttempt = [];
+var userAttempt.push(currentInput);
+var letterToSearch = userAttempt.pop();
+var letterFound = false;
+
+
+var i = 0;
+while (i < secretWord.length) {
+	if screctWord[i] === letterToSearch{
+		letterFound = true;
+		console.log (letterFound);
+	}
+	i = i +1;
+}
+console.log(letterFound);
+return "letterFound";
+
+
